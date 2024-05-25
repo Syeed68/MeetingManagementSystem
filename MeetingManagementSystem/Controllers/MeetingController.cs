d9ad8bc88943bd5da23c0ccfbf605dec0e285a53
Syeed68 <abu.syeed968@gmail.com>
2024-05-25 12:53:01 +0600

    commit for modified project.

--- MeetingManagementSystem/Controllers/MeetingController.cs
+++ MeetingManagementSystem/Controllers/MeetingController.cs
@@ -2,6 +2,9 @@
 using MeetingManagementSystem.Models.Entities;
 using MeetingManagementSystem.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace MeetingManagementSystem.Controllers
 {
@@ -17,20 +20,53 @@ namespace MeetingManagementSystem.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Create(VMmeetingMinutesMaster MeetingMinutesMaster)
+        public async Task<IActionResult> Create(VMmeetingMinutesMaster vm)
         {
-            await _db.AddAsync(new MeetingMinutesMaster
+            ;
+            string ReturnSTR = "";
+            var resultParameter = new SqlParameter("@ReturnMSG", SqlDbType.VarChar, 300)
             {
-                CustomerId= MeetingMinutesMaster.CustomerId,
-                Date=MeetingMinutesMaster.Date,
-                Time = MeetingMinutesMaster.Time,
-                MeetingPlace= MeetingMinutesMaster.MeetingPlace,
-                AttendsFromClientSide = MeetingMinutesMaster.AttendsFromClientSide,
-                AttendsFromHostSide= MeetingMinutesMaster.AttendsFromHostSide,
-                MeetingAgenda = MeetingMinutesMaster.MeetingAgenda,
-                MeetingDiscussion = MeetingMinutesMaster.MeetingDiscussion,
-                MeetingDecision = MeetingMinutesMaster.MeetingDecision
-            });
+                Direction = ParameterDirection.Output
+            };
+            try
+            {
+                DateTime time;
+                DateTime.TryParse(vm.Time, out time);
+                _db.Database.ExecuteSqlRaw("Meeting_Minutes_Master_Tbl @CustomerId, @Date, @Time, @MeetingPlace, @AttendsFromClientSide, @AttendsFromHostSide, @MeetingAgenda, @MeetingDiscussion, @MeetingDecision, @ReturnMSG out",
+               new SqlParameter("@CustomerId", vm.CustomerId),
+               new SqlParameter("@Date", vm.Date),
+               new SqlParameter("@Time", time),
+               new SqlParameter("@MeetingPlace", vm.MeetingPlace),
+               new SqlParameter("@AttendsFromClientSide", vm.AttendsFromClientSide),
+               new SqlParameter("@AttendsFromHostSide", vm.AttendsFromHostSide),
+               new SqlParameter("@MeetingAgenda", vm.MeetingAgenda),
+               new SqlParameter("@MeetingDiscussion", vm.MeetingDiscussion),
+               new SqlParameter("@MeetingDecision", vm.MeetingDecision),
+               resultParameter);
+               ReturnSTR = (string)resultParameter.Value;
+
+                if(ReturnSTR!= "Failed")
+                {
+                    foreach(var i in vm.Products)
+                    {
+                        _db.Database.ExecuteSqlRaw("SPMeetingMinutesDetails @MeetingMinutesMasterId,@ProductId, @Quantity",
+                        new SqlParameter("@MeetingMinutesMasterId", Convert.ToInt32(ReturnSTR)),
+                        new SqlParameter("@ProductId", i.ProductId),
+                        new SqlParameter("@Quantity", i.Quantity));
+                    }
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+           
+
+            
+
+
             return Json("Success");
         }
     }
